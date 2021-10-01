833599b1feb9fdae11d42fdc865243d0bf6fa8a4
Joey Carter <joey.snarrcarter@gmail.com>
2021-10-01 09:35:30 -0400

    Bump to version v0.1.8

--- atlasplots/__version__.py
+++ atlasplots/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "atlasplots"
 __description__ = "ATLAS Plots for Humans"
 __url__ = "https://atlas-plots.readthedocs.io"
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 __author__ = "Joey Carter"
 __author_email__ = "joey.carter@cern.ch"
 __license__ = "MIT"
